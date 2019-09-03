0bdd1f1c4e108f1dc3118f9121599d417a12497d
Alex Wang <jianuo.wang@duke.edu>
2019-09-02 22:31:19 -0400

    added button functionality and question checking

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     //Place your instance variables here
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -36,21 +37,38 @@ class ViewController: UIViewController {
         }
         
         checkAnswer()
+        
+        questionNumber += 1
+        
+        nextQuestion()
     }
     
     
     func updateUI() {
 
     }
-    
+
 
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+            print("End of quiz")
+            questionNumber = 0
+        }
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            print("Wrong!")
+        }
     }
     
     
